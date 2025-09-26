27d3cdc866d7c082179155f5b201d847736911fb
qadroid <34576771+Qadroid@users.noreply.github.com>
2025-09-26 14:46:59 +0100

    Some cleaning and CSS changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@
 	let { children } = $props();
 </script>
 
-<div class="h-screen w-screen flex flex-col bg-gray-100">
+<div class="h-screen w-screen flex flex-col dark text-slate-200">
 	{@render children()}
 </div>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+</style>
