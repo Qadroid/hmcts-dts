b831fbc0c9efdaac41f39a575e415ed37e066f81
qadroid <34576771+Qadroid@users.noreply.github.com>
2025-09-26 14:48:33 +0100

    Created components to display tasks from DB data

---
+++ src/lib/components/TasksView.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button/index.ts";
+	import Separator from "$lib/components/ui/separator/separator.svelte";
+	import * as Card from "$lib/components/ui/card/index.ts";
+    import TaskView from "$lib/components/TaskView.svelte";
+
+	let { tasks } = $props();
+</script>
+
+<div class="flex flex-col p-3">
+	<Card.Root>
+		<Card.Header class="">
+			<Card.Title>All Tasks</Card.Title>
+			<Card.Description>List of tasks with details and status</Card.Description>
+			<Card.Action>
+				<Button variant="outline" href="/tasks/?new">New Task</Button>
+			</Card.Action>
+			<Separator class="my-4" />
+		</Card.Header>
+		<Card.Content>
+			{#each tasks as task}
+				<TaskView {task} />
+			{/each}
+		</Card.Content>
+	</Card.Root>
+</div>
